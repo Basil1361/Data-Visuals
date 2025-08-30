52d865c6033fd14d362b8eda40b5a262b81d138e
Basil1361 <trainerbasil69@gmail.com>
2025-08-30 18:26:57 +0800

    Added dataset and updated graph/datasets scripts

--- DataVisuals/File/Datasets.py
+++ DataVisuals/File/Datasets.py
@@ -2,5 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"D:\Advanced\DataVisuals\Source\Toyota.csv")
-print(df.to_string())
+print(df.head(5)) 
+print(df.tail(20)) 
+
+
 
